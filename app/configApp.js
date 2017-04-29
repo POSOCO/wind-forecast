35bc80c5629939a168b5925f8fb8680db7a61328
NagaSudhir Pulla <nagasudhirpulla@gmail.com>
2017-04-29 22:19:04 +0530

    Update configApp.js

--- app/configApp.js
+++ app/configApp.js
@@ -3,4 +3,4 @@ var data_strategy_ = "csv";
 var secs_ = 15 * 60;
 var type_ = "average";
 var serverBaseAddress_ = "http://localhost:62448";
-var eDNAPnt_ = "WRLDC.PHASOR.WRDC0783";
+var eDNAPnt_ = "WRLDCMP.SCADA1.A0047236";
